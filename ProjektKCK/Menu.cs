8e4bfc4e1ea3bb8f888b8b5f30675d993b70f320
Krzysztof Kordiukiewiczi <krzysztof.kordiukiewicz@gmail.com>
2023-10-18 18:11:06 +0200

    Menu

---
+++ ProjektKCK/Menu.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using static System.Console;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace menu
+{
+    class Menu
+    {
+
+        private int indeks;
+        private string[] opcje;
+        private string Prompt;
+        public Menu(string prompt, string[] options)
+        {
+            Prompt = prompt;
+            opcje = options;
+            indeks = 0;
+        }
+        public void WypiszOpcje()
+        {
+            WriteLine(Prompt);
+            for (int i = 0; i < opcje.Length; i++)
+            {
+                string ObecnaOpcja = opcje[i];
+                string pre;
+                if (i == indeks)
+                {
+                    pre = "*";
+                    ForegroundColor = ConsoleColor.Black;
+                    BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    pre = " ";
+                    ForegroundColor = ConsoleColor.White;
+                    BackgroundColor = ConsoleColor.Black;
+                }
+                WriteLine($"{pre}<<{ObecnaOpcja}>>");
+
+            }
+            ResetColor();
+        }
+        public int Run()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                Clear();
+                WypiszOpcje();
+                ConsoleKeyInfo przyciskinfo = ReadKey(true);
+                keyPressed = przyciskinfo.Key;
+                if(keyPressed == ConsoleKey.UpArrow)
+                {
+                    indeks--;
+                    if(indeks == -1)
+                    {
+                        indeks = opcje.Length - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    indeks++;
+                    if (indeks == opcje.Length)
+                    {
+                        indeks = 0; 
+                    }
+                }
+            } while (keyPressed != ConsoleKey.Enter);
+
+            return indeks;
+        }
+        
+    }
+}
--- ProjektKCK/Program.cs
+++ ProjektKCK/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using menu;
+
+namespace ProjektPO
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            while (true)
+            {
+                string prompt = "Giereczka";
+                
+                string[] options = { "Graj", "Ustawienia (Moze jakies beda)", "Wyjdz"};
+                Menu mainmenu = new Menu(prompt, options);
+                int indeks = mainmenu.Run();
+                WriteLine(" ");
+                int wroc = 0;
+                switch (indeks)
+                {
+                    case 0:
+                        
+                        break;
+                    case 1:
+                        
+                        break;
+                    case 2:
+                        System.Environment.Exit(0);
+                        break;
+                }
+            }
+            
+        }
+    }
+}
+
+
+
+
